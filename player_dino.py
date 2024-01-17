70799effa51f871230216a373664416cc801ecfb
kisel <kiselevaeva377@gmail.com>
2024-01-17 16:54:52 +0300

    игрок

---
+++ player_dino.py
@@ -0,0 +1,94 @@
+from pygame import *
+import pyganim
+
+MOVE_SPEED = 7
+WIDTH = 22
+HEIGHT = 32
+COLOR = "#888888"
+JUMP_POWER = 10
+GRAVITY = 0.35  # Сила, которая будет тянуть нас вниз
+ANIMATION_DELAY = 0.1
+
+ANIMATION_RIGHT = [('images/Pink_Monster.png'),
+                   ('images/Pink_Monster_Walk_6.png')]
+
+
+class Player(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0  # скорость перемещения. 0 - стоять на месте
+        self.startX = x  # Начальная позиция Х, пригодится когда будем переигрывать уровень
+        self.startY = y
+        self.yvel = 0  # скорость вертикального перемещения
+        self.onGround = False  # На земле ли я?
+        self.image = Surface((32, 32))
+        self.image.fill(Color(COLOR))
+        self.rect = Rect(x, y, WIDTH, HEIGHT)  # прямоугольный объект
+        self.image.set_colorkey(Color(COLOR))  # делаем фон прозрачным
+        #        Анимация движения вправо
+        boltAnim = []
+        for anim in ANIMATION_RIGHT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+        self.boltAnimRight = pyganim.PygAnimation(boltAnim)
+        self.boltAnimRight.play()
+        #        Анимация движения влево
+        self.boltAnimStay = pyganim.PygAnimation([('images/Pink_Monster.png', 0.1)])
+        self.boltAnimStay.play()
+        self.boltAnimStay.blit(self.image, (0, 0))  # По-умолчанию, стоим
+
+        self.boltAnimJumpRight = pyganim.PygAnimation([('images/Pink_Monster_Climb_4.png', 0.1)])
+        self.boltAnimJumpRight.play()
+
+        self.boltAnimJump = pyganim.PygAnimation([('images/Pink_Monster_Jump_8.png', 0.1)])
+        self.boltAnimJump.play()
+
+    def update(self, left, right, up, platforms):
+
+        if up:
+            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
+                self.yvel = -JUMP_POWER
+            self.image.fill(Color(COLOR))
+            self.boltAnimJump.blit(self.image, (0, 0))
+
+        if right:
+            self.xvel = MOVE_SPEED  # Право = x + n
+            self.image.fill(Color(COLOR))
+            if up:
+                self.boltAnimJumpRight.blit(self.image, (0, 0))
+            else:
+                self.boltAnimRight.blit(self.image, (0, 0))
+
+        if not (left or right):  # стоим, когда нет указаний идти
+            self.xvel = 0
+            if not up:
+                self.image.fill(Color(COLOR))
+                self.boltAnimStay.blit(self.image, (0, 0))
+
+        if not self.onGround:
+            self.yvel += GRAVITY
+
+        self.onGround = False;  # Мы не знаем, когда мы на земле((
+        self.rect.y += self.yvel
+        self.collide(0, self.yvel, platforms)
+
+        self.rect.x += self.xvel  # переносим свои положение на xvel
+        self.collide(self.xvel, 0, platforms)
+
+    def collide(self, xvel, yvel, platforms):
+        for p in platforms:
+            if sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
+
+                if xvel > 0:  # если движется вправо
+                    self.rect.right = p.rect.left  # то не движется вправо
+
+                if xvel < 0:  # если движется влево
+                    self.rect.left = p.rect.right  # то не движется влево
+
+                if yvel > 0:  # если падает вниз
+                    self.rect.bottom = p.rect.top  # то не падает вниз
+                    self.onGround = True  # и становится на что-то твердое
+                    self.yvel = 0  # и энергия падения пропадает
+
+                if yvel < 0:  # если движется вверх
+                    self.rect.top = p.rect.bottom  # то не движется вверх
+                    self.yvel = 0  # и энергия прыжка пропадает
